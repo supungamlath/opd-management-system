1453111f0080fa66cb8497a5e56557ddb3fa3dd5
PRO-GUNE <chathuragunasekera@gmail.com>
2023-03-11 14:51:00 +0530

    set up sign in and sign up

--- back-end/src/services/utils.js
+++ back-end/src/services/utils.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 const {secret} = require('../config/config')
 
 // Hashing the passwords
-const signToken = (payload) => jwt.sign(payload, secret)
+const signToken = (payload) => jwt.sign(payload, secret, {expiresIn: 604800})
 const verifyToken = (token) => jwt.verify(token, secret, (err, res) => {
     if(err) throw err
     return res
