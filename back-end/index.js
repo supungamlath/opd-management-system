053b06689b8c5627d03d8f351508c31cc1e661eb
Isuru Gunarathne <isuru623@gmail.com>
2023-03-11 09:59:07 +0530

    add basic back end

---
+++ back-end/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const {server_config} = require("./src/config/config")
+const app = express()
+const port = server_config.port
+
+// Use the body-parser middleware to parse the request body
+app.use(bodyParser.json());
+// Accept these headers to avoid CORS errors on the client side
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Authorization, Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+///////////////////// Routes /////////////////////
+// Default Route
+app.get('/', (req, res) => res.send("Welcome to Kernel Panic, gammac neh!"))
+
+// log listining port
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ back-end/src/config/config.js
@@ -0,0 +1,10 @@
+const server_config = {
+    port: process.env.PORT || 5000
+}
+
+const secret = 'secret'
+
+module.exports = {
+    server_config,
+    secret
+}
