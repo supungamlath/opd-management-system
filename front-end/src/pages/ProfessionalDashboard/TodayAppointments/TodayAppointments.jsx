f757815e5b8c88ef122175906322b8e33c848844
Isuru Gunarathne <isuru623@gmail.com>
2023-03-12 05:06:47 +0530

    add pro dashboard

--- front-end/src/pages/ProfessionalDashboard/TodayAppointments/TodayAppointments.jsx
+++ front-end/src/pages/ProfessionalDashboard/TodayAppointments/TodayAppointments.jsx
@@ -19,8 +19,8 @@ function TodayAppointments() {
   const columns = [
     { field: 'id', headerName: 'ID', width: 90 },
     {
-      field: 'docName',
-      headerName: 'Doctor Name',
+      field: 'patientName',
+      headerName: 'Patient Name',
       width: 150,
       editable: false,
     },
@@ -57,9 +57,9 @@ function TodayAppointments() {
   },]
 
   const rows = [
-    { id: 1, docName: 'Kamal', date: '11/12/2023', time: "10:30 AM", status: "Pending" },
-    { id: 2, docName: 'Kamal', date: '11/12/2023', time: "11:30 AM", status: "Pending" },
-    { id: 3, docName: 'Kamal', date: '11/12/2023', time: "12:30 AM", status: "Pending" },
+    { id: 1, patientName: 'Kamal', date: '11/12/2023', time: "10:30 AM", status: "Pending" },
+    { id: 2, patientName: 'Amal', date: '11/12/2023', time: "11:30 AM", status: "Pending" },
+    { id: 3, patientName: 'Sunimal', date: '11/12/2023', time: "12:30 AM", status: "Pending" },
   ];
 
   const usersActions = (params) => (
