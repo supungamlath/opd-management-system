2cfb7a1ae57e9b71fa2202e27afd4716d809d439
Isuru Gunarathne <isuru623@gmail.com>
2023-03-11 22:31:40 +0530

    finish new appointment

--- front-end/src/pages/NewAppointment/NewAppointment.jsx
+++ front-end/src/pages/NewAppointment/NewAppointment.jsx
@@ -1,26 +1,31 @@
 import React, { useState } from 'react'
+import './newAppointment.scss'
 
 function NewAppointment() {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [date, setDate] = useState('')
+    const [time, setTime] = useState('')
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        console.log(email)
-        console.log(password)
+        console.log(date)
+        console.log(time)
     }
     return (
-        <div>
-            <h1>New Appointment</h1>
+        <div className='all'>
+            <h1 className='title'>New Appointment</h1>
             <div className='login-window'>
                 <form className='login-form' onSubmit={handleSubmit}>
-                    <label htmlFor='email'>Email</label>
-                    <input value={email} onChange={(e) => setEmail(e.target.value)} type='email' placeholder='you@uom.lk' id="email" name="email" />
-                    <label htmlFor='password'>Password</label>
-                    <input value={password} onChange={(e) => setPassword(e.target.value)} type='password' placeholder='***********' id="password" name="password" />
-                    <button type="submit">Log In</button>
+                    <div className='in-line'>
+                        <label htmlFor='date'>Date</label>
+                        <input value={date} onChange={(e) => setDate(e.target.value)} type='date' placeholder='dd/mm/yyyy' id="date" name="date" />
+                    </div>
+                    <div className='in-line'>
+                        <label htmlFor='time'>Time</label>
+                        <input value={time} onChange={(e) => setTime(e.target.value)} type='time' placeholder='***********' id="time" name="time" />
+                    </div>
+                    <button type="submit" style ={{borderRadius: '0.5rem',backgroundColor : '#3b8dff', color : 'white', minWidth : 300, minHeight : 60}}>confirm edit</button>
                 </form>
-                
+
             </div>
         </div>
     );
