e501e538628646879d6ba71dd98fdc044bc13466
PRO-GUNE <chathuragunasekera@gmail.com>
2023-03-11 16:25:03 +0530

    updated passwords

--- back-end/data/models/dml.sql
+++ back-end/data/models/dml.sql
@@ -1,7 +1,9 @@
 USE OPDdb;
 
 INSERT INTO user (Username, Password, Role) VALUES
-('admin', '$2a$10$CwTycUXWue0Thq9StjUM0uQ4dVJ4kdSBYfX8R1mXHu0dyj16Fc.ja', 'System Admin'), -- password=admin123
+('admin', '$2a$10$CwTycUXWue0Thq9StjUM0uQ4dVJ4kdSBYfX8R1mXHu0dyj16Fc.ja', 'System Admin'), -- password - admin123
+('johnDoe', '$2a$10$CwTycUXWue0Thq9StjUM0uMFnO06ozXGXxrQcSM7aHnSUdwrEDG/u', 'Patient'), -- password - password123
+('janeSmith', '$2a$10$CwTycUXWue0Thq9StjUM0uI9IfHw/yXeJwyos8R.HMupooqnU8GGm', 'Healthcare Professional'); -- password - password456
 
 INSERT INTO system_admin (user_ID, first_name, last_name, email, phone_number) VALUES
 (1, 'Anoshan', 'Jayahanthan', 'johndoe@example.com', '123-456-7890');
