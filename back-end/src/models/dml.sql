0da5f35728daeeed92870605a12bea9825b3bbe8
Isuru Gunarathne <isuru623@gmail.com>
2023-03-12 03:07:23 +0530

    hash pw for patient

--- back-end/src/models/dml.sql
+++ back-end/src/models/dml.sql
@@ -5,7 +5,7 @@ INSERT INTO system_admin (username, password, first_name, last_name, email, phon
 ('Admin','$2a$10$CwTycUXWue0Thq9StjUM0uMFnO06ozXGXxrQcSM7aHnSUdwrEDG&#x2F;u','admin','test','admin@gmail.com','0751234568'); -- password : password123
 
 INSERT INTO patient (username, password, first_name, last_name, nic, address, email, gender, dob) VALUES
-('JohnDoe', '123456', 'John', 'Doe', '1234567890', '123 Main St', 'janesmith@example.com', 'Female', '1990-01-01');
+('JohnDoe', '$2a$10$CwTycUXWue0Thq9StjUM0uG8BnUfbEgXpwGSWmzmOTa7NZl7gLMK2', 'John', 'Doe', '1234567890', '123 Main St', 'janesmith@example.com', 'Female', '1990-01-01'); -- password : 123456
 
 INSERT INTO healthcare_professional (username, password, first_name, last_name, email, phone_Number, role) VALUES
 ('Chathura', '123456', 'Chathura', 'Gunasekara', 'chathura@example.com', '555-555-5555', 'Doctor');
