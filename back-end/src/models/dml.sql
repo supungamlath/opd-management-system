78258276e994e8108e887233f8e9e50b657fb4d6
Supun Gamlath <86224008+supungamlath@users.noreply.github.com>
2023-03-11 23:25:45 +0530

    Rename professional_ID in dml

--- back-end/src/models/dml.sql
+++ back-end/src/models/dml.sql
@@ -9,10 +9,10 @@ INSERT INTO patient (username, password, first_name, last_name, nic, address, em
 INSERT INTO healthcare_professional (username, password, first_name, last_name, email, phone_Number, role) VALUES
 ('Chathura', '123456', 'Chathura', 'Gunasekara', 'chathura@example.com', '555-555-5555', 'Doctor');
 
-INSERT INTO patient_record (patient_ID, healthcare_Professional_ID, disease_Reason, prescriptions, treatment_Plans, progress_Notes) VALUES
+INSERT INTO patient_record (patient_ID, professional_ID, disease_Reason, prescriptions, treatment_Plans, progress_Notes) VALUES
 (1, 1, 'Fever', 'Ibuprofen', 'Rest and hydration', 'Patient feels better after taking medication');
 
 
-INSERT INTO appointment (appointment_Date, appointment_Time, patient_ID, healthcare_professional_ID, status) VALUES
+INSERT INTO appointment (appointment_Date, appointment_Time, patient_ID, professional_ID, status) VALUES
 ('2023-03-15', '10:00:00', 1, 1, 'Pending'),
 ('2023-03-20', '14:30:00', 1, 1, 'Accepted');
