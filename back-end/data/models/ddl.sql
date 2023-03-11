936dbd9886a164a3d7c7caa19e074853d8cb9d6e
Anoshan Jayahanthan <anoshan265@gmail.com>
2023-03-11 15:32:27 +0530

    add dml

--- back-end/data/models/ddl.sql
+++ back-end/data/models/ddl.sql
@@ -11,64 +11,64 @@ CREATE TABLE `user` (
   PRIMARY KEY (`user_ID`)
 );
 
-CREATE TABLE `System_Admin` (
-  `Admin_ID` INT NOT NULL AUTO_INCREMENT,
-  `User_ID` INT,
-  `First_Name` varchar(25) NOT NULL,
-  `Last_Name` varchar(25) NOT NULL,
-  `Email` varchar(100) NOT NULL UNIQUE,
-  `Phone_Number` varchar(15),
-  PRIMARY KEY (`Admin_ID`),
-  FOREIGN KEY (`User_ID`) REFERENCES `User`(`User_ID`)  
+CREATE TABLE `system_admin` (
+  `admin_ID` INT NOT NULL AUTO_INCREMENT,
+  `user_ID` INT,
+  `first_name` varchar(25) NOT NULL,
+  `last_Name` varchar(25) NOT NULL,
+  `email` varchar(100) NOT NULL UNIQUE,
+  `phone_number` varchar(15),
+  PRIMARY KEY (`admin_iD`),
+  FOREIGN KEY (`user_iD`) REFERENCES `user`(`user_id`)  
 );
 
-CREATE TABLE `Patient` (
-  `Patient_ID` INT NOT NULL AUTO_INCREMENT,
-  `User_ID` INT,
-  `First_Name` varchar(25) NOT NULL,
-  `Last_Name` varchar(25) NOT NULL,
-  `NIC` varchar(20) NOT NULL UNIQUE,
-  `Address` varchar(80) NOT NULL,
-  `Email` varchar(100) NOT NULL UNIQUE,
-  `Gender` ENUM('Male', 'Female', 'Other') NOT NULL,
-  `DOB` DATE,
-  PRIMARY KEY (`Patient_ID`),
-  FOREIGN KEY (`User_ID`) REFERENCES `User`(`User_ID`)
+CREATE TABLE `patient` (
+  `patient_ID` INT NOT NULL AUTO_INCREMENT,
+  `user_ID` INT,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `nic` varchar(20) NOT NULL UNIQUE,
+  `address` varchar(80) NOT NULL,
+  `email` varchar(100) NOT NULL UNIQUE,
+  `gender` ENUM('Male', 'Female', 'Other') NOT NULL,
+  `dob` DATE,
+  PRIMARY KEY (`patient_ID`),
+  FOREIGN KEY (`user_ID`) REFERENCES `user`(`user_ID`)
 );
 
-CREATE TABLE `Healthcare_Professional` (
-  `Healthcare_Professional_ID` INT NOT NULL AUTO_INCREMENT,
-  `User_ID` INT NOT NULL,
-  `First_Name` varchar(25) NOT NULL,
-  `Last_Name` varchar(25) NOT NULL,
-  `Email` varchar(100) NOT NULL UNIQUE,
-  `Phone_Number` varchar(15) NOT NULL,
-  `Role` varchar(30) NOT NULL,
-  PRIMARY KEY (`Healthcare_Professional_ID`),
-  FOREIGN KEY (`User_ID`) REFERENCES `User`(`User_ID`)
+CREATE TABLE `healthcare_professional` (
+  `healthcare_professional_ID` INT NOT NULL AUTO_INCREMENT,
+  `user_ID` INT NOT NULL,
+  `first_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `email` varchar(100) NOT NULL UNIQUE,
+  `phone_number` varchar(15) NOT NULL,
+  `role` varchar(30) NOT NULL,
+  PRIMARY KEY (`healthcare_professional_ID`),
+  FOREIGN KEY (`user_ID`) REFERENCES `user`(`user_ID`)
 );
 
-CREATE TABLE `Patient_Record` (
-  `Record_ID` INT NOT NULL AUTO_INCREMENT,
-  `Patient_ID` INT NOT NULL,
-  `Healthcare_Professional_ID` INT NOT NULL,
-  `Disease_Reason` varchar(40) NOT NULL,
-  `Prescriptions` varchar(200) NOT NULL,
-  `Treatment_Plans` varchar(200) NOT NULL,
-  `Progress_Notes` varchar(200) NOT NULL,
-  PRIMARY KEY (`Record_ID`),
-  FOREIGN KEY (`Patient_ID`) REFERENCES `Patient`(`Patient_ID`),
-  FOREIGN KEY (`Healthcare_Professional_ID`) REFERENCES `Healthcare_Professional`(`Healthcare_Professional_ID`)
+CREATE TABLE `patient_record` (
+  `record_ID` INT NOT NULL AUTO_INCREMENT,
+  `patient_ID` INT NOT NULL,
+  `healthcare_Professional_ID` INT NOT NULL,
+  `disease_Reason` varchar(40) NOT NULL,
+  `prescriptions` varchar(200) NOT NULL,
+  `treatment_Plans` varchar(200) NOT NULL,
+  `progress_Notes` varchar(200) NOT NULL,
+  PRIMARY KEY (`record_ID`),
+  FOREIGN KEY (`patient_ID`) REFERENCES `patient`(`patient_ID`),
+  FOREIGN KEY (`healthcare_professional_ID`) REFERENCES `healthcare_professional`(`healthcare_professional_ID`)
 );
 
-CREATE TABLE `Appointment` (
-  `Appointment_ID` INT NOT NULL AUTO_INCREMENT,
-  `Appointment_Date` DATE NOT NULL,
-  `Appointment_Time` TIME NOT NULL,
-  `Patient_ID` INT NOT NULL,
-  `Healthcare_Professional_ID` INT NOT NULL,
+CREATE TABLE `appointment` (
+  `appointment_ID` INT NOT NULL AUTO_INCREMENT,
+  `appointment_Date` DATE NOT NULL,
+  `appointment_Time` TIME NOT NULL,
+  `patient_ID` INT NOT NULL,
+  `healthcare_professional_ID` INT NOT NULL,
   `status` ENUM('Pending', 'Accepted', 'Declined', 'Cancelled', 'Completed'),
-  PRIMARY KEY (`Appointment_ID`),
-  FOREIGN KEY (`Patient_ID`) REFERENCES `Patient`(`Patient_ID`),
-  FOREIGN KEY (`Healthcare_Professional_ID`) REFERENCES `Healthcare_Professional`(`Healthcare_Professional_ID`) ON DELETE CASCADE ON UPDATE CASCADE
+  PRIMARY KEY (`appointment_ID`),
+  FOREIGN KEY (`patient_ID`) REFERENCES `patient`(`patient_ID`),
+  FOREIGN KEY (`healthcare_Professional_ID`) REFERENCES `healthcare_professional`(`healthcare_professional_ID`) ON DELETE CASCADE ON UPDATE CASCADE
 );
