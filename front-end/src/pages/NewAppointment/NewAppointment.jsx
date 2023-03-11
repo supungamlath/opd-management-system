ae36ddfd6f7bddf81db9a265e986d39c778b5abc
Isuru Gunarathne <isuru623@gmail.com>
2023-03-12 03:12:58 +0530

    update newAppointment

--- front-end/src/pages/NewAppointment/NewAppointment.jsx
+++ front-end/src/pages/NewAppointment/NewAppointment.jsx
@@ -6,6 +6,10 @@ function NewAppointment() {
     const [time, setTime] = useState('')
     const [doctor, setDoctor] = useState('')
 
+    // get doctors
+    // const { res } = useApi("/api/doctor/get_doctors", "GET")
+
+
     const handleSubmit = (e) => {
         e.preventDefault()
 
@@ -21,10 +25,10 @@ function NewAppointment() {
                 <form className='login-form' onSubmit={handleSubmit}>
                     <div className='in-line'>
                         <label htmlFor='doctor'>Doctor</label>
-                        <select  className = 'selectWindow'value={doctor} onChange={(e) => setDoctor(e.target.value)} id="doctor" name="doctor">
-                            <option value="doctor1">Doctor 1</option>
-                            <option value="doctor2">Doctor 2</option>
-                            <option value="doctor3">Doctor 3</option>
+                        <select className='selectWindow' value={doctor} onChange={(e) => setDoctor(e.target.value)} id="doctor" name="doctor">
+                            <option value="1">Dr. Kamal</option>
+                            <option value="2">Dr. Ahmed</option>
+                            <option value="3">Dr. Ali</option>
                         </select>
                     </div>
                     <div className='in-line'>
@@ -35,7 +39,7 @@ function NewAppointment() {
                         <label htmlFor='time'>Time</label>
                         <input value={time} onChange={(e) => setTime(e.target.value)} type='time' placeholder='***********' id="time" name="time" />
                     </div>
-                    <button type="submit" style={{ borderRadius: '0.5rem', backgroundColor: '#3b8dff', color: 'white', minWidth: 300, minHeight: 60 }}>confirm edit</button>
+                    <button type="submit" style={{ borderRadius: '0.5rem', backgroundColor: '#3b8dff', color: 'white', minWidth: 300, minHeight: 60 }}>confirm appointment</button>
                 </form>
 
             </div>
